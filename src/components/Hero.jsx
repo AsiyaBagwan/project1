5543edf1bce104d69267c141ecc8ec3111b48441
AsiyaBagwan <asiyabagwan06@gmail.com>
2024-10-24 11:35:59 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+const Hero = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>
+                    YOUR FEET DESERVES THE BEST
+                </h1>
+                <p className="caption">YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES</p>
+                <div className="hero-btn">
+                    <button>Shop now</button>
+                    <button className="secondary-btn">Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also Available on</p>
+                    <div className="brnad-icons">
+                        <img src="/images/shops.png"/>
+                    </div>
+                </div>
+            </div>
+            <div className="hero-image">
+            <img src="/images/shoe_image.png"/>
+            </div>
+
+
+
+        </main>
+
+
+
+
+    )
+
+
+};
+export default Hero;
